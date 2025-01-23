7fe4210a8e255f9dccfe6cba724767c0e6c09427
Andrea Piccinelli <andrea8395@gmail.com>
2025-01-23 09:40:25 -0500

    py3 changes

--- scripts/install_cmssw.sh
+++ scripts/install_cmssw.sh
@@ -33,7 +33,9 @@ function setup_cmssw {
     scram p CMSSW ${cmssw_ver}
 
     cd ${cmssw_ver}/src
-    git clone git@github.com:sscruz/cmgtools-lite.git -b 104X_dev_nano_lepMVA CMGTools
+    #git clone git@github.com:sscruz/cmgtools-lite.git -b 104X_dev_nano_lepMVA CMGTools
+    #git clone https://github.com/jdelrieg/topEFT_ttHMVA_Run3.git -b newcmgtools_python3 CMGTools
+    git clone https://github.com/apiccine/topEFT_ttHMVA_Run3.git -b nd_run3 CMGTools
     git clone https://github.com/cms-nanoAOD/nanoAOD-tools.git PhysicsTools/NanoAODTools
     echo "Getting CMS ENV from ${PWD}"
     eval $(scramv1 runtime -sh)
--- setup.py
+++ setup.py
@@ -12,27 +12,27 @@ def main():
     abs_path = subprocess.check_output(["git","rev-parse","--show-toplevel"])
     abs_path = abs_path.strip()
 
-    cmssw_release = "CMSSW_10_6_19_patch2"
-    scram_arch = "slc7_amd64_gcc700"
+    cmssw_release = "CMSSW_14_0_6"
+    scram_arch = "el9_amd64_gcc12"
 
 
     if os.path.exists("topcoffea"):
-        print "topcoffea directory already installed, skipping this part\n"
+        print("topcoffea directory already installed, skipping this part\n")
     else:
-        print "Installing topcoffea cfg and json directories"
+        print("Installing topcoffea cfg and json directories")
         topcoffea_url = "https://github.com/TopEFT/topcoffea.git"
         tag = "master"
         prj_head = "{}/topcoffea".format(abs_path)
         cfg_dir  = "topcoffea/cfg"
         json_dir = "topcoffea/json"
         subprocess.check_call(["./scripts/install_configs.sh",topcoffea_url,prj_head,tag,cfg_dir,json_dir])
-        print ""
+        print("")
 
     if os.path.exists(cmssw_release):
-        print "CMSSW release {} detected, skipping this part".format(cmssw_release)
+        print("CMSSW release {} detected, skipping this part".format(cmssw_release))
     else:
-        print "Setting up CMSSW release and getting NanoAODTools"
+        print("Setting up CMSSW release and getting NanoAODTools")
         subprocess.check_call(["./scripts/install_cmssw.sh",abs_path,cmssw_release,scram_arch])
 
-    print "\nDone!\nMake sure to do a cmsenv before activating and/or using lobster!"
+    print("\nDone!\nMake sure to do a cmsenv before activating and/or using lobster!")
 main()
